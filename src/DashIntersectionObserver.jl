ea57c9f32ad0e3deae6aa3d19485b76342aecc84
RenaudLN <renaudlaine31@gmail.com>
2023-07-29 19:18:55 +1000

    version update

--- src/DashIntersectionObserver.jl
+++ src/DashIntersectionObserver.jl
@@ -3,7 +3,7 @@ module DashIntersectionObserver
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "1.0.0"
+const version = "1.0.1"
 
 include("jl/dashintersectionobserver.jl")
 
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_intersection_observer.js",
-    external_url = "https://unpkg.com/dash_intersection_observer@1.0.0/dash_intersection_observer/dash_intersection_observer.js",
+    external_url = "https://unpkg.com/dash_intersection_observer@1.0.1/dash_intersection_observer/dash_intersection_observer.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_intersection_observer.js.map",
-    external_url = "https://unpkg.com/dash_intersection_observer@1.0.0/dash_intersection_observer/dash_intersection_observer.js.map",
+    external_url = "https://unpkg.com/dash_intersection_observer@1.0.1/dash_intersection_observer/dash_intersection_observer.js.map",
     dynamic = true,
     async = nothing,
     type = :js
